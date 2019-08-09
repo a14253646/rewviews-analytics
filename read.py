a70febda0c1a03ec07399e342bdb3bfc90d1a2c0
Will <a14253646@gmail.com>
2019-08-09 23:36:03 +0800

    新增篩選小於100的留言

--- read.py
+++ read.py
@@ -1,17 +1,27 @@
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 = 0
+		if count % 1000 == 0:
 			print(len(data))
-print('檔案讀取完成, 共有' , len(data), 筆)
+print('檔案讀取完成,ㄧ共有' , len(data), '筆資料')
 
-sun_len = 0
+sum_len = 0
 for d in data:
 		len(d)
 		sum_len = sum_len + len(d)
 print('每筆平均長度為',sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '比留言小於100')		
+import random
+r = random.randint(1,1000)
+print(new[r])
 
 
 
